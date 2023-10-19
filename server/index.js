e5ef257a8673252eaf80a71ad3a6956c4fe62c10
felixjkw <post2felix@gmail.com>
2023-10-19 14:00:32 +0200

    first commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server} = require("socket.io");
+const { Socket } = require("dgram");
+app.use(cors());
+
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) =>{
+    console.log(socket.id);
+
+    socket.on("disconnet", ()=>{
+        console.log("User disconented", socket.id)
+    })
+});
+
+server.listen(3001, () =>{
+    console.log("SERVER RUNNING")
+});
