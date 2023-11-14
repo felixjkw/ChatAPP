d225df264218fb5de06918ff4af434f1cf3c4767
felixjkw <post2felix@gmail.com>
2023-11-14 16:52:36 +0100

    display messages with css

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -16,6 +16,7 @@ function Chat({socket, username, room }) {
             };
 
             await socket.emit("send_message", messageData);
+            setMessageList((list) => [...list, messageData])
         }
     };
 
@@ -34,9 +35,21 @@ function Chat({socket, username, room }) {
                 <p>Live Chat</p>
             </div>
             <div className="chat-body">
-            {memoizedMessageList.map((messageContent, index) => (
-          <h1 key={index}>{messageContent.message}</h1>
-        ))}
+            {memoizedMessageList.map((messageContent, index) => {
+                return(
+                <div className="message">
+                    <div>
+                        <div className="message-content">
+                            <p>{messageContent.message}</p>
+                        </div>
+                        <div className="message-meta">
+                            <p>{messageContent.time}</p>
+                            <p>{messageContent.author}</p>
+                        </div>
+                    </div>
+                </div>
+                );
+                })}
             </div>
             <div className="chat-footer">
                 <input type="text" placeholder="Hey..." onChange={(event) => {setCurrentMessage(event.target.value);
